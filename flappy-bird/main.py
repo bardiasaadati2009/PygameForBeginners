58943e9a09dbc6c962dfb91d53068cba67a9d7ae
Bardia <102543327+bardiasaadati2009@users.noreply.github.com>
2022-03-31 22:01:32 +0430

    Update main.py

--- flappy-bird/main.py
+++ flappy-bird/main.py
@@ -1,93 +1,91 @@
 import sys
+import random
 import time
 
 import pygame
-import random
 
 pygame.init()
+
+pipEvent = pygame.USEREVENT + 1
+pygame.time.set_timer(pipEvent, 1000)
 birdEvent = pygame.USEREVENT
 pygame.time.set_timer(birdEvent, 100)
-pipeEvent = pygame.USEREVENT + 1
-pygame.time.set_timer(pipeEvent, 1000)
 
 fps = 90
 width = 400
-height = 800
-floorX = 0
+height = 710
+floorx = 0
 birdMove = 0
-grav = 0.2
+grav = 0.15
 pipeList = []
 game_status = True
-score = 0
+Score = 0
 activeScore = True
 highScore = 0
 
-btn = pygame.transform.scale(pygame.image.load("assets/img/btn.png"),(120,50))
-btnRect = btn.get_rect(center=(190, 415))
+btn = pygame.transform.scale(pygame.image.load("assets/img/flappyBirdPlayButton.png"), (120, 50))
+btnRect = btn.get_rect(center=(200, 470))
 
-bg = pygame.transform.scale(pygame.image.load("assets/img/bg2.png"), (width, height))
+bg = pygame.transform.scale(pygame.image.load('assets/img/bg2.png'), (width, height))
 floorImage = pygame.transform.scale(pygame.image.load('assets/img/floor.png'), (width, 200))
-pipe = pygame.image.load("assets/img/pipe_green.png")
+pipe = pygame.image.load('assets/img/pipe_green.png')
+jump = pygame.mixer.Sound('assets/sound/smb_stomp.wav')
+gameOVER = pygame.mixer.Sound('assets/sound/smb_mariodie.wav')
+gameOver = pygame.image.load('assets/img/message.png')
+gameOverR = gameOver.get_rect(center=(200, 350))
 
-gameOverImage = pygame.image.load("assets/img/message.png")
-gameOverImageRect = gameOverImage.get_rect(center=(190, 300))
-
-winSound = pygame.mixer.Sound('assets/sound/smb_stomp.wav')
-gameOverSound = pygame.mixer.Sound('assets/sound/smb_mariodie.wav')
-
-bird_up = pygame.image.load("assets/img/red_bird_up_flap.png")
-bird_mid = pygame.image.load("assets/img/red_bird_mid_flap.png")
-bird_down = pygame.image.load("assets/img/red_bird_down_flap.png")
-
-font = pygame.font.Font("assets/font/Flappy.TTF", 40)
+GameOver = pygame.mixer.Sound('assets/sound/smb_mariodie.wav')
+bird_up = pygame.image.load('assets/img/red_bird_up_flap.png')
+bird_mid = pygame.image.load('assets/img/red_bird_mid_flap.png')
+bird_down = pygame.image.load('assets/img/red_bird_down_flap.png')
+font = pygame.font.Font("assets/font/Flappy.TTF", 20)
 
 bird_list = [bird_up, bird_mid, bird_down]
 bird_index = 0
+
 bird = bird_list[bird_index]
 
-birdRect = bird.get_rect(center=(50, 300))
+birdRect = bird.get_rect(center=(50, 450))
 win = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 
 def generatePipeRect():
-    rand_pos = random.randrange(350, 600)
-    pipRect = pipe.get_rect(midtop=(400, rand_pos))
-    pipRectUp = pipe.get_rect(midbottom=(400, rand_pos - 150))
-    return pipRect, pipRectUp
+    rand_pos = random.randrange(350, 500)
+    pipeRect = pipe.get_rect(midtop=(300, rand_pos))
+    pipeRectUp = pipe.get_rect(midbottom=(300, rand_pos - 170))
+    return pipeRect, pipeRectUp
 
 
-def movePipe(pipes):
+def MovePipe(pipes):
     for pipeRect in pipes:
-        pipeRect.centerx -= 3
+        pipeRect.centerx -= 4
         if pipeRect.right < 0:
             pipes.remove(pipeRect)
+
     return pipes
 
 
 def drawPipes(pipes):
-    for piperect in pipes:
-        if piperect.bottom > 450:
-            win.blit(pipe, piperect)
+    for pipeRect in pipes:
+        if pipeRect.bottom > 345:
+            win.blit(pipe, pipeRect)
         else:
             revImage = pygame.transform.rotate(pipe, 180)
-            win.blit(revImage, piperect)
+            win.blit(revImage, pipeRect)
 
 
-def checkCollision(pipes):
-    # global activeScore
+def checkCollsion(pipes):
     for pipeRect in pipes:
         if birdRect.colliderect(pipeRect):
-            # activeScore=True
-            gameOverSound.play()
-            time.sleep(3)
+            gameOVER.play()
+            time.sleep((2))
             return False
-        if birdRect.top <= -20 or birdRect.bottom >= 600:
-            # activeScore=True
-            print(pipes)
-            gameOverSound.play()
-            time.sleep(3)
+        if birdRect.top <= -20 or birdRect.bottom >= 570:
+            gameOVER.play()
+            time.sleep((2))
             return False
+
     return True
 
 
@@ -99,31 +97,30 @@ def birdAnime():
 
 def displayScore(status):
     if (status == "active"):
-        text = font.render(str(score), True, (255, 255, 255))
-        textRect = text.get_rect(center=(185, 100))
+        text = font.render(str(Score), False, (255, 255, 255))
+        textRect = text.get_rect(center=(200, 100))
         win.blit(text, textRect)
     if status == "game over":
-        # score
-        text = font.render("Score: " + str(score), True, (255, 255, 255))
-        textRect = text.get_rect(center=(185, 100))
+        # Score
+        text = font.render("Score = " + str(Score), False, (255, 255, 255))
+        textRect = text.get_rect(center=(200, 100))
         win.blit(text, textRect)
-        # high score
-        text2 = font.render("High Score: " + str(highScore), True, (255, 255, 255))
-        textRect2 = text2.get_rect(center=(190, 500))
+        # high Score
+        text2 = font.render("High Score = " + str(highScore), False, (255, 255, 255))
+        textRect2 = text2.get_rect(center=(200, 150))
         win.blit(text2, textRect2)
 
 
 def updateScore():
-    global highScore
-    global score, activeScore
+    global Score, activeScore, highScore
     for pipeRect in pipeList:
         if 40 < pipeRect.centerx < 60 and activeScore:
-            score += 1
+            Score += 1
             activeScore = False
         if pipeRect.centerx < 40:
             activeScore = True
-    if score > highScore:
-        highScore = score
+    if Score > highScore:
+        highScore = Score
 
 
 run = True
@@ -134,21 +131,19 @@ while run:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                winSound.play()
                 birdMove = 0
-                birdMove -= 5
-            if event.key == pygame.K_t:
-                game_status = True
-                birdMove = 0
-                score = 0
-                pipeList.clear()
-                birdRect.center = (50, 300)
-        pos=pygame.mouse.get_pos()
+                birdMove -= 4
+                jump.play()
+        pos = pygame.mouse.get_pos()
         if btnRect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0]:
-                print("left btn")
+                game_status = True
+                birdMove = 0
+                pipeList.clear()
+                birdRect.center = (50, 300)
+                Score = 0
 
-        if event.type == pipeEvent:
+        if event.type == pipEvent:
             pipeList.extend(generatePipeRect())
         if event.type == birdEvent:
             if bird_index < 2:
@@ -157,25 +152,29 @@ while run:
                 bird_index = 0
             bird, birdRect = birdAnime()
 
-    floorX -= 1
+    pipeList = MovePipe(pipeList)
+    floorx -= 1
     birdMove += grav
     birdRect.centery += birdMove
-    pipeList = movePipe(pipeList)
-
     win.blit(bg, (0, 0))
+    win.blit(floorImage, (floorx, 570))
+    win.blit(floorImage, (floorx + width, 570))
     if game_status:
-        game_status = checkCollision(pipeList)
+        game_status = checkCollsion(pipeList)
         drawPipes(pipeList)
         win.blit(bird, birdRect)
-        win.blit(floorImage, (floorX, 650))
-        win.blit(floorImage, (floorX + width, 650))
+        win.blit(floorImage, (floorx, 570))
+        win.blit(floorImage, (floorx + width, 570))
         updateScore()
         displayScore("active")
     else:
         displayScore("game over")
-        win.blit(gameOverImage, gameOverImageRect)
-        win.blit(btn,btnRect)
-    if floorX <= -400:
-        floorX = 0
+        win.blit(gameOver, gameOverR)
+
+        win.blit(btn, btnRect)
+        jump.stop()
+    if floorx <= -400:
+        floorx = 0
+
     pygame.display.update()
     clock.tick(fps)
